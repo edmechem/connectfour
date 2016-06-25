a30bbe460d7d235d84f34d498cb4d8d7c1d12177
Ed Mechem <email": null>
2016-06-25 16:47:34 -0700

    Add functions for getting clicked column & evaluating validity

--- application.js
+++ application.js
@@ -3,21 +3,65 @@ $(document).ready(function(){
 });
 
 var Game = function() {
+  // var winner = "";
   this.buildBoard();
   // repeat loop until someone wins, starts here
+    currentGame = this;
     player = this.whoseTurn();
-    console.log(player);
+    var statusMessage = "";
+    player == "R" ? statusMessage = "Red, " : statusMessage = "Black, ";
+    statusMessage += "it's your turn! Click a column to play your piece."
+    this.updateStatusMessage(statusMessage);
+    this.updateMiniPiece(player);
+    $(".piece").on("click", function(event) {
+        event.preventDefault();
+        clickedPiece = $(this)[0];
+        clickedClass = $(clickedPiece).attr("class");
+        clickedColumn = clickedClass.charAt(clickedClass.indexOf("col") + 3);
+        var isValid = currentGame.isValidColumn(clickedColumn);
+
+        if (!isValid) {
+          currentGame.displayInvalidColumnMessage();
+        } else {
+          currentGame.updateStatusMessage("Clicked column = " + clickedColumn);
+          // it's a valid column, place their piece!
+          // currentGame.placePiece()
+        }
+
+    });
+
   // end repeat loop here
+};
+
+// Game.prototype.placePiece
+
+Game.prototype.displayInvalidColumnMessage = function() {
+  var statusMessage = ""
+  player == 'R' ? statusMessage += "Red, " : statusMessage += "Black, ";
+  statusMessage += "that's not a valid column! Please try again."
+  currentGame.updateStatusMessage(statusMessage);
+};
+
+Game.prototype.isValidColumn = function(column) {
+  var columnValues = currentGame.getColumnValues(clickedColumn);
+  var emptyPieces = columnValues.filter(function(piece) {return piece == ""});
+  return emptyPieces.length > 0;
+};
+
+Game.prototype.getColumnValues = function(column) {
+  outputColumn = [];
+  currentGame.board.forEach(function(row){outputColumn.push(row[column]);})
+  return outputColumn;
 };
 
 Game.prototype.buildBoard = function() {
   this.board = [
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""]
+    ["", "", "R", "", "", "", ""],
+    ["", "", "R", "", "", "", ""],
+    ["", "", "R", "", "", "", ""],
+    ["", "", "R", "", "", "", ""],
+    ["", "", "R", "", "", "", ""],
+    ["", "", "R", "", "", "", ""]
   ];
 };
 
@@ -25,8 +69,25 @@ Game.prototype.whoseTurn = function() {
   var flattenedBoard = [].concat.apply([], this.board);
   var redCount = flattenedBoard.filter(function(piece) { return piece == "R" }).length;
   var blackCount = flattenedBoard.filter(function(piece) { return piece == "B" }).length;
-  return redCount > blackCount ? "B" : "R";
+  return redCount <= blackCount ? "R" : "B";
 };
+
+Game.prototype.updateStatusMessage = function(statusMessage) {
+  $('.status_message').text(statusMessage);
+};
+
+Game.prototype.updateMiniPiece = function(player) {
+  if (player == "R") {
+    $(".mini_piece").removeClass("white");
+    $(".mini_piece").removeClass("black");
+    $(".mini_piece").addClass("red")
+  } else {
+    $(".mini_piece").removeClass("white");
+    $(".mini_piece").removeClass("red");
+    $(".mini_piece").addClass("black")
+  }
+}
+
 
 
 
