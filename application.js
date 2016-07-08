15e018f6a226f14894af05d40516269bc769acbf
Ed Mechem <ed@mechem.org>
2016-07-07 17:48:49 -0700

    Add Diagonal wins; refactor & cleanup

--- application.js
+++ application.js
@@ -3,48 +3,81 @@ $(document).ready(function(){
 });
 
 var Game = function() {
-  this.buildBoard();
   currentGame = this;
   currentGame.winner = "";
   currentGame.errorMessage = "";
+  buildBoardArray();
   evaluateTurnUpdateStatus();
   clickHandler();
 };
 
+function buildBoardArray() {
+  currentGame.board = [
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""]
+  ];
+};
+
 function evaluateTurnUpdateStatus () {
-  player = currentGame.whoseTurn();
-  // debugger;
+  player = whoseTurn();
   if (currentGame.errorMessage != "") {
-    currentGame.updateStatusMessage(currentGame.errorMessage);
+    updateStatusMessage(currentGame.errorMessage);
   } else {
     var statusMessage = "";
     player == "R" ? statusMessage = "Red, " : statusMessage = "Black, ";
     statusMessage += "it's your turn! Click a column to play your piece."
-    currentGame.updateStatusMessage(statusMessage);
+    updateStatusMessage(statusMessage);
   };
-  currentGame.updateMiniPiece(player);
+  updateMiniPiece(player);
+}
+
+function whoseTurn () {
+  var flattenedBoard = [].concat.apply([], currentGame.board);
+  var redCount = flattenedBoard.filter(function(piece) { return piece == "R" }).length;
+  var blackCount = flattenedBoard.filter(function(piece) { return piece == "B" }).length;
+  return redCount <= blackCount ? "R" : "B";
+};
+
+function updateStatusMessage(statusMessage) {
+  $('.status_message').text(statusMessage);
+};
+
+function updateMiniPiece(player) {
+  if (player == "R") {
+    $(".mini_piece").removeClass("white");
+    $(".mini_piece").removeClass("black");
+    $(".mini_piece").addClass("red")
+  } else {
+    $(".mini_piece").removeClass("white");
+    $(".mini_piece").removeClass("red");
+    $(".mini_piece").addClass("black")
+  }
 }
 
 function clickHandler () {
   $(".piece").on("click", function(event) {
       event.preventDefault();
       if (currentGame.winner == "") {
-        clickedPiece = $(this)[0];
-        clickedClass = $(clickedPiece).attr("class");
-        clickedColumn = clickedClass.charAt(clickedClass.indexOf("col") + 3);
-        columnValues = currentGame.getColumnValues(clickedColumn);
-        var isValid = currentGame.isValidColumn();
+        var clickedPiece = $(this)[0];
+        var clickedClass = $(clickedPiece).attr("class");
+        var clickedColumn = clickedClass.charAt(clickedClass.indexOf("col") + 3);
+        columnValues = getColumnValues(clickedColumn);
+        var isValid = isValidColumn(columnValues);
         if (!isValid) {
           displayInvalidColumnMessage();
         } else {
           currentGame.errorMessage = "";
-          currentGame.placePiece();
-          currentGame.updateBoardArray();
+          placePiece(clickedColumn, columnValues);
+          updateBoardArray(clickedColumn, columnValues);
         }
         evaluateWinner();
         if (currentGame.winner != "") {
           console.log("Winner is: " + currentGame.winner)
-          currentGame.updateStatusMessage("Congratulations, " + currentGame.winner + "! You Win!");
+          updateStatusMessage("Congratulations, " + currentGame.winner + "! You Win!");
         } else {
           evaluateTurnUpdateStatus();
         }
@@ -52,17 +85,62 @@ function clickHandler () {
     });
 }
 
-function evaluateWinner() {
-  evaluateHorizontalWin();
-  evaluateVerticalWin();
-  evaluateSlashDiagonalWin();
-  // evaluateBackSlashDiagonalWin();
+function getColumnValues(column) {
+  outputColumn = [];
+  currentGame.board.forEach(function(row){outputColumn.push(row[column]);})
+  return outputColumn;
+};
+
+function isValidColumn(columnValues) {
+  var emptyPieces = columnValues.filter(function(piece) {return piece == ""});
+  return emptyPieces.length > 0;
+};
+
+function displayInvalidColumnMessage() {
+  if (currentGame.errorMessage == "") {
+    player == 'R' ? currentGame.errorMessage += "Red, " : currentGame.errorMessage += "Black, ";
+    currentGame.errorMessage += "the column's full! Try a different one :)";
+    currentGame.updateStatusMessage(currentGame.errorMessage);
+  }
+};
+
+function placePiece(clickedColumn, columnValues) {
+  row = "row" + lowestOpenRow(columnValues);
+  col = "col" + clickedColumn;
+  cssSelector = "." + row + "." + col;
+  if (player == "R") {
+    $(cssSelector).removeClass("white");
+    $(cssSelector).addClass("red");
+  } else {
+    $(cssSelector).removeClass("white");
+    $(cssSelector).addClass("black");
+  };
+};
+
+function lowestOpenRow(columnValues) {
+  for (var i = 0; i < columnValues.length; i++) {
+    if (columnValues[i] != "") {return i-1;}
+  }
+  return i-1;
+};
+
+function updateBoardArray(clickedColumn, columnValues) {
+  row = lowestOpenRow(columnValues);
+  col = parseInt(clickedColumn);
+  currentGame.board[row][col] = player;
 }
 
-function evaluateHorizontalWin() {
-  currentGame.board.forEach(function(row){
+function evaluateWinner() {
+  evaluateWin(currentGame.board);                             // horizontal
+  evaluateWin(transpose(currentGame.board));                  // vertical
+  evaluateWin(diagonalize(currentGame.board));                // forward-slash diagonal
+  evaluateWin(diagonalize(reverseBoard(currentGame.board)));  // back-slash diagonal
+}
+
+function evaluateWin(board) {
+  board.forEach(function(row){
     rowString = row.join('.');
-    console.log(rowString);
+    // console.log(rowString);
     if (rowString.indexOf("R.R.R.R") != -1) {
       currentGame.winner = "Red";
     } else if (rowString.indexOf("B.B.B.B") != -1) {
@@ -79,197 +157,73 @@ function transpose(array) {
   });
 }
 
-function evaluateVerticalWin() {
-  var board = transpose(currentGame.board)
+function reverseBoard(board) {
+  var reversedBoard = [];
   board.forEach(function(row){
-    rowString = row.join('.');
-    console.log(rowString);
-    if (rowString.indexOf("R.R.R.R") != -1) {
-      currentGame.winner = "Red";
-    } else if (rowString.indexOf("B.B.B.B") != -1) {
-      currentGame.winner = "Black";
-    }
+    var newRow = row.slice();
+    var reversedRow = newRow.reverse();
+    reversedBoard.push(reversedRow);
   })
+  return reversedBoard;
 }
 
-function evaluateSlashDiagonalWin() {
-  var board = currentGame.board;
-  var diagonalBoard = [];
+function diagonalize(board) {
+  newBoard = [];
 
+  // numRows = board.length;    // 6
+  // numCols = board[0].length; // 7
+  // constraint = 4;
+  // ultimately, the more scalable solution would do nested loops using vars above;
+  // for now we'll manually build it.
+
+  row1 = [
+    board[3][0],
+    board[2][1],
+    board[1][2],
+    board[0][3]
+  ]
+
+  row2 = [
+    board[4][0],
+    board[3][1],
+    board[2][2],
+    board[1][3],
+    board[0][4]
+  ]
+
+  row3 = [
+    board[5][0],
+    board[4][1],
+    board[3][2],
+    board[2][3],
+    board[1][4],
+    board[0][5]
+  ]
+
+  row4 = [
+    board[5][1],
+    board[4][2],
+    board[3][3],
+    board[2][4],
+    board[1][5],
+    board[0][6]
+  ]
+
+  row5 = [
+    board[5][2],
+    board[4][3],
+    board[3][4],
+    board[2][5],
+    board[1][6]
+  ]
+
+  row6 = [
+    board[5][3],
+    board[4][4],
+    board[3][5],
+    board[2][6]
+  ]
+
+  newBoard = [row1, row2, row3, row4, row5, row6]
+  return newBoard;
 }
-
-function buildSlashBoard() {
-  var board = currentGame.board;
-  var slashBoard = [];
-
-  var cols = 7;
-  var rows = 6;
-  var min = 4;
-
-  for (var i = ((rows - min) + 1); i < rows; i++) {
-
-
-
-  }
-
-}
-
-
-
-
-
-Game.prototype.updateBoardArray = function() {
-  row = currentGame.lowestOpenRow();
-  col = parseInt(clickedColumn);
-  currentGame.board[row][col] = player;
-}
-
-Game.prototype.placePiece = function() {
-  row = "row" + currentGame.lowestOpenRow();
-  col = "col" + clickedColumn;
-  cssSelector = "." + row + "." + col;
-  if (player == "R") {
-    $(cssSelector).removeClass("white");
-    $(cssSelector).addClass("red");
-  } else {
-    $(cssSelector).removeClass("white");
-    $(cssSelector).addClass("black");
-  };
-};
-
-Game.prototype.lowestOpenRow = function() {
-  for (var i = 0; i < columnValues.length; i++) {
-    if (columnValues[i] != "") {return i-1;}
-  }
-  return i-1;
-};
-
-function displayInvalidColumnMessage () {
-  if (currentGame.errorMessage == "") {
-    player == 'R' ? currentGame.errorMessage += "Red, " : currentGame.errorMessage += "Black, ";
-    currentGame.errorMessage += "the column's full! Try a different one :)";
-    // debugger;
-    currentGame.updateStatusMessage(currentGame.errorMessage);
-  }
-};
-
-Game.prototype.isValidColumn = function() {
-  var emptyPieces = columnValues.filter(function(piece) {return piece == ""});
-  return emptyPieces.length > 0;
-};
-
-Game.prototype.getColumnValues = function(column) {
-  outputColumn = [];
-  currentGame.board.forEach(function(row){outputColumn.push(row[column]);})
-  return outputColumn;
-};
-
-Game.prototype.buildBoard = function() {
-  this.board = [
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""]
-  ];
-};
-
-Game.prototype.whoseTurn = function() {
-  var flattenedBoard = [].concat.apply([], this.board);
-  var redCount = flattenedBoard.filter(function(piece) { return piece == "R" }).length;
-  var blackCount = flattenedBoard.filter(function(piece) { return piece == "B" }).length;
-  return redCount <= blackCount ? "R" : "B";
-};
-
-Game.prototype.updateStatusMessage = function(statusMessage) {
-  $('.status_message').text(statusMessage);
-};
-
-Game.prototype.updateMiniPiece = function(player) {
-  if (player == "R") {
-    $(".mini_piece").removeClass("white");
-    $(".mini_piece").removeClass("black");
-    $(".mini_piece").addClass("red")
-  } else {
-    $(".mini_piece").removeClass("white");
-    $(".mini_piece").removeClass("red");
-    $(".mini_piece").addClass("black")
-  }
-}
-
-
-
-
-
-//  JQuery - play button event listener here (or somewhere)
-
-
-
-// Game.prototype.playGame = function() {
-
-//   var currentColor = this.whoseTurn();
-//   var boardRow = 3;
-
-//   for (i=0; i<10; i++) {
-//     var chosenColumn = prompt("Which column do you want?\nPick a number from 1 to 4");
-//     var placed = false;
-//       while (placed === false) {
-//       };
-//       var slot = this.currentGame[boardRow][chosenColumn - 1];
-
-//       if (slot === "") {
-//         this.currentGame[boardRow][chosenColumn - 1] = currentColor;
-//         console.log(this.currentGame[boardRow][chosenColumn - 1]);
-
-//         console.log(this.currentGame);
-//         placed = true;
-//       } else {
-
-//         if (boardRow == 0) {
-//           alert("This column is full");
-//           console.log(this.currentGame);
-//           placed = true;
-//         } else {
-//           boardRow -= 1;
-//         };
-
-//       };
-
-//   };
-
-// };
-
-// Game.prototype.redWins = "R" * 4;
-// Game.prototype.blackWins = "B" * 4;
-
-// Game.prototype.horizontalWin = function(){
-
-//   for(i = 0; i < this.currentGame.length; i++){
-//     if (this.currentGame[i].join() === this.redWins) {
-//       return "Red Wins"
-//     } else if (this.currentGame[i].join() === this.blackWins) {
-//       return "Black Wins"
-//     }
-//   }
-// };
-
-// Game.prototype.verticalWin = function(){
-
-//   var transpose = function(a)  {
-//     return a[0].map(function (_, c) { return a.map(function (r) { return r[c]; }); });
-//   }
-//   transpose(this.currentGame);
-//   for(i = 0; i < this.currentGame.length; i++){
-//     if (this.currentGame[i].join() === this.redWins) {
-//       return "Red Wins"
-//     } else if (this.currentGame[i].join() === this.blackWins) {
-//       return "Black Wins"
-//     }
-//   }
-// };
-
-// Game.prototype.diagonalWin = function(){
-
-// };
-
